66a864a1a0c85a4f7261541df2d7ddbb4013fd46
Pavel Patrusov <pavelp9415@gmail.com>
2019-08-08 23:50:22 +0300

    Completed unit 2.4

---
+++ module_02/PlayerCharacter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace module_02
+{
+    class PlayerCharacter
+    {
+        public PlayerCharacter()
+        {
+            DateOfBirth = DateTime.MinValue; // Magic number
+            DaysSinceLastLogin = -1; // Magic number
+        }
+
+        public string Name { get; set; }
+
+        public int DaysSinceLastLogin { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+    }
+}
---
+++ module_02/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace module_02
+{
+    class Program
+    {
+        static void Main()
+        {
+            var player = new PlayerCharacter();
+            player.Name = "Sarah";
+            player.DaysSinceLastLogin = 42;
+
+            PlayerDisplayer.Write(player);
+        }
+    }
+}
